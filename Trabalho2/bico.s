05ee464d7a17dd3ad2314072f1004a31d4ddf7c2
Giovanni Mesquita Micaroni <g231702@dac.unicamp.br>
2022-12-08 15:41:32 -0300

    passando em todas, falta filter 1d

--- Trabalho2/bico.s
+++ Trabalho2/bico.s
@@ -105,26 +105,26 @@ gets:
 .globl atoi
 atoi:
     li t0, ' '
-    mv a1, a0
-    li t3, 0
+    mv a1, a0 #string em a1
+    li t3, 0 
     li t4, 10
     li t6, '0'
     1:
-        lb t2, 0(a1) 
-        addi a1, a1, 1
-        bne t2, t0, 2f
+        lb t2, 0(a1) #byte da string em t2
+        addi a1, a1, 1 #vai pro proximo byte
+        bne t2, t0, 2f #se o byte nao é espaço, pula
         j 1b
     2:
         li t0, '0'
         li t1, '9'
         li t5, 0
     3:
-        beq t2, t5, 5f
-        blt t2, t0, 4f
-        bgt t2, t1, 4f
-        mul t3, t3, t4
-        sub t2, t2, t6
-        add t3, t3, t2
+        beq t2, t5, 5f #se o byte é o \0, pula pra 5
+        blt t2, t0, 4f #se o byte é menor que '0', pula para 4
+        bgt t2, t1, 4f #se o byte é maior que '9', pula para 4
+        mul t3, t3, t4 #multiplica o valor guardado por 10
+        sub t2, t2, t6 #subtrai '0' do byte lido
+        add t3, t3, t2 #adiciona o valor do byte lido menos '0' ao valor guardado
     4:
         lb t2, 0(a1)
         addi a1, a1, 1
@@ -136,68 +136,84 @@ atoi:
 
 
 
-
-
 .globl itoa
 itoa:
-    mv t0, a2 #base em t0
-    mv t1, a0 #numero em t1; buffer em a1 já
-    mv a4, t1
-    mv t4, a1
-    mv a5, a1
+
+    #a0 -> valor; a1 -> buffer; a2 ->base
     li t2, 10
     li t3, 0
-    li t6, 0
-    li t5, 0
+    mv t0, a0 #valor inteiro em t0
+    mv t4, a1 #buffer em t4
+    mv a6, a1 #buffer em a6
+    beq a2, t2, decimal #se a base for decimal, vai pro rótulo decimal
+    j hexa
 
-    conta:
-        divu a4, a4, t0
-        beq a4, t3, 3f
-        addi t5, t5, 1
-        j conta
-    3:
-        add t4, t4, t5
-    1:
-        beq t4, a1, 4f
-        rem t6, t1, t0
-        blt t6, t2, menor_10
-        addi t6, t6, -10
-        addi t6, t6, 'a'
-        j 2f
+    decimal:
+        beq t0, t3, inverte #se o resultado da divisao for 0, acabou e basta inverter a string
+        rem t1, t0, t2 #resto da divisao do numero atual por 10 em t1
+        addi t1, t1, '0' #soma '0' ao resto da divisao para ter o caracter do numero em t1
+        sb t1, 0(t4) #guarda o caracter no buffer
+        divu t0, t0, t2 #divide o numero atual por 10 e guarda nele mesmo
+        addi t4, t4, 1 #soma um ao endereço do buffer
+        j decimal
 
-        menor_10:
-            addi t6, t6, '0'
-
-    2:
-        sb t6, 0(t4)
-        divu t1, t1, t0
-        addi t4, t4, -1
+    hexa:
+        li t2, 16
+        li t5, 10
+        1:
+        beq t0, t3, inverte #se o resultado da divisao for 0, acabou e basta inverter a string
+        rem t1, t0, t2 #resto da divisao do numero atual por 16 em t1
+        blt t1, t5, 2f #se o resto da divisao for menor que 10, vai para 2f
+        addi t1, t1, -10 #caso contrario, subtrai 10 do resto e soma com o caracter
+        addi t1, t1, 'a' # 'a', para obter a letra representativa do valor em hexadecimal
+        j 3f
+        2:
+        addi t1, t1, '0' #se o resto for menor que 10, basta somar o caracter '0'
+        3:
+        sb t1, 0(t4) #guarda o caracter no buffer
+        divu t0, t0, t2 #divide o numero atual por 16 e guarda nele mesmo
+        addi t4, t4, 1 #soma um ao endereço do buffer
         j 1b
-
-    4:
-        mv a0, a5
-        ret
-
-
-
-
-
     
+    inverte:
+        sub t1, t4, a1 #calcula quantos digitos foram adicionados a string
+        srli t5, t1, 1 #divide esse numero de digitos por 2, que será o numero de trocas necessárias
+        mv t6, t4
+        li t3, 0
+        addi t4, t4, -1
+        4:
+        beq t3, t5, cabou #se o numero de trocas necessarias tiver sido feito, acabou
+        lb a4, 0(t4) #carrega o valor do final da array nao trocada em a4
+        lb a5, 0(a1) #carrega o valor inicial da array nao trocada em a5
+        sb a4, 0(a1) #guarda o valor final da array nao trocada no inicio dela
+        sb a5, 0(t4) #guarda o valor inicial da array nao trocada no fim dela
+        addi t4, t4, -1 #subtrai 1 do fim da array
+        addi a1, a1, 1 #soma 1 ao inicio da array
+        addi t3, t3, 1 #soma 1 ao numero de trocas
+        j 4b
+
+    cabou:
+        li t0, 0
+        sb t0, 0(t6)
+        mv a0, a6 #move o valor inicial da array para a0
+        ret
 
 .globl sleep
 sleep:
-    addi sp, sp, -4
+    addi sp, sp, -8
     sw ra, 0(sp)
     mv t0, a0 #tempo de sleep em t0
     jal get_time
     add t0, a0, t0 #tempo de sleep + tempo inicial em t0
     1:
         bgt a0, t0, 2f
+        sw ra, 4(sp)
         jal get_time
+        lw ra, 4(sp)
         j 1b
     2:
         lw ra, 0(sp)
-        addi sp, sp, 4
+        addi sp, sp, 8
         ret
 
 .globl approx_sqrt
--- Trabalho2/main.s
+++ Trabalho2/main.s
@@ -3,11 +3,12 @@
 .align 4
 .globl main
 main:
-    li a0, 121
-    li a1, 10
-    jal approx_sqrt
+    li a0, 123
+    la a1, buffer
+    li a2, 10
+    jal itoa
     hahaha:
 
 
 .align 4
-buffer: .ascii "HELLO\0"
+buffer: .skip 0x20
